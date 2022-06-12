5f64abbcba22f31e1a078205a3848ccbc052fbae
armorDLV <59936969+armorDLV@users.noreply.github.com>
2022-06-12 15:47:48 +0200

    Create flashcards_1.py

---
+++ flashcards_1.py
@@ -0,0 +1,7 @@
+def flash_card(term, definition) -> None:
+    print('Card:', term, sep='\n')
+    print('Definition:', definition, sep='\n')
+
+
+if __name__ == '__main__':
+    flash_card('hyperaphia', 'extraordinarily keen sense of touch')
